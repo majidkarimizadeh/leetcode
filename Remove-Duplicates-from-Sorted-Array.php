30322cca2687a6797abcf9c1918a5816d82b04c7
Majid Karimizadeh <mj.karimizadeh@gmail.com>
2022-05-22 20:16:42 +0200

    Remove-Duplicates-from-Sorted-Array added

---
+++ Remove-Duplicates-from-Sorted-Array.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+        
+        $nums = array_unique($nums);
+        return count($nums);
+    }
+}
